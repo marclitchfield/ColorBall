1e57a2dab22f268123a5ed1497c3346f329dd81b
Marc Litchfield <marclitchfield@gmail.com>
2013-01-26 10:35:50 -0800

    Fixed problems with reading imu data. Introduced tinycolor dependency for hsv-rgb conversion

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-EnergyBall
-==========
+## ColorBall
+
+node program that receives streaming IMU (pitch, acceleration, yaw) data from a Sphero and changes its color in response. Two modes are supported:
+
+* hsv - (the default) yaw controls hue. saturation and brightness are fixed at 100%.
+* rgb - roll controls red, pitch controls green, yaw controls blue 
+
+
+## Usage
+
+    node colorball.js [rgb | hsv]
+
+
+## SDK
+
+node-sphero sdk is from boundsj/node-sphero
+
--- colorball.js
+++ colorball.js
@@ -1,4 +1,5 @@
 var spheroModule = require('node-sphero');
+var tinycolor = require('tinycolor2');
 var sphero = new spheroModule.Sphero();
 
 sphero.on('connected', function() {
@@ -12,99 +13,39 @@ sphero.on('connected', function() {
         ]);
 });
 
-sphero.on('error', function(message) {
-	console.log(message);
-});
-
 sphero.on('notification', function(message) {
         var imuData = {
-                pitch: readValue(message.DATA, 0),
-                roll: readValue(message.DATA, 2),
-                yaw: readValue(message.DATA, 4)
+                pitch: message.DATA.readInt16BE(0),
+                roll: message.DATA.readInt16BE(2),
+                yaw: message.DATA.readInt16BE(4)
         };
 
-        var h =  imuData.yaw + 180, s = 100, l = 100;
-            //s = (imuData.pitch + 180)/360*100.0,
-            //l = (imuData.yaw + 180)/360*100.0;
+        var colorFunc = process.argv[0] === 'rgb' ? imuRgb : imuHsv;
+        var color = colorFunc(imuData);
 
-        var color = hslToRgb(h, s, l);
-        sphero.setRGBLED(color[0], color[1], color[2]);
+        sphero.setRGBLED(color.r, color.g, color.b);
 
-        console.log(h, s, l, color[0], color[1], color[2]);
+        console.log(imuData, color.r, color.g, color.b);
 });
 
-function readValue(data, pos) {
-        return data.readInt8(pos) << 8 | data.readInt8(pos + 1);
+function imuHsv(imuData) {
+    return tinycolor({ 
+        h: imuData.yaw + 180, 
+        s: 100,
+        v: 100 
+    }).toRgb();
 }
 
-function hslToRgb(h, s, v) {
-    var r, g, b;
-    var i;
-    var f, p, q, t;
-    
-    // Make sure our arguments stay in-range
-    h = Math.max(0, Math.min(360, h));
-    s = Math.max(0, Math.min(100, s));
-    v = Math.max(0, Math.min(100, v));
-    
-    // We accept saturation and value arguments from 0 to 100 because that's
-    // how Photoshop represents those values. Internally, however, the
-    // saturation and value are calculated from a range of 0 to 1. We make
-    // That conversion here.
-    s /= 100;
-    v /= 100;
-    
-    if(s == 0) {
-        // Achromatic (grey)
-        r = g = b = v;
-        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+function imuRgb(imuData) {
+    function toComponent(degree) {
+        return (180 - Math.abs(degree)) / 180 * 255;
     }
-    
-    h /= 60; // sector 0 to 5
-    i = Math.floor(h);
-    f = h - i; // factorial part of h
-    p = v * (1 - s);
-    q = v * (1 - s * f);
-    t = v * (1 - s * (1 - f));
 
-    switch(i) {
-        case 0:
-            r = v;
-            g = t;
-            b = p;
-            break;
-            
-        case 1:
-            r = q;
-            g = v;
-            b = p;
-            break;
-            
-        case 2:
-            r = p;
-            g = v;
-            b = t;
-            break;
-            
-        case 3:
-            r = p;
-            g = q;
-            b = v;
-            break;
-            
-        case 4:
-            r = t;
-            g = p;
-            b = v;
-            break;
-            
-        default: // case 5:
-            r = v;
-            g = p;
-            b = q;
-    }
-    
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    return {
+        r: toComponent(imuData.roll),
+        g: toComponent(imuData.pitch),
+        b: toComponent(imuData.yaw)
+    };
 }
 
 sphero.connect();
